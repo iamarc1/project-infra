a88b2d5495d9e5ee132c0a5c7bf23668ac94af4e
Aishwary Rudra Chaturvedi <59038905+iamarc1@users.noreply.github.com>
2025-04-21 20:43:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI for N. Virginia
   instance_type = "t2.micro"
 }
